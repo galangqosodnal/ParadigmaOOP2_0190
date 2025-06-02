14bc088779778c16e06126c639eb0f700f718881
galangqosodnal <mgalangyk@gmail.com>
2025-06-02 15:46:21 +0700

    Coba panggil method hasil override dari derivedClass

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -14,3 +14,10 @@ class derivedClass : public baseClass {
         cout << "Halo saya Function dari derived Class";
     }
 };
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
