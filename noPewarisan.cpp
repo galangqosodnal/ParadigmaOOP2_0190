b643944a56f9b40e31ba6eeed44fd8f52976a7bf
galangqosodnal <mgalangyk@gmail.com>
2025-06-02 15:39:26 +0700

    Tambahkan class derivedClass dan override fungsi perkenalan

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -7,3 +7,10 @@ class baseClass {
         cout << "Halo saya Function dari base class";
     }
 };
+
+class derivedClass : public baseClass {
+    public:
+    void perkenalan() {
+        cout << "Halo saya Function dari derived Class";
+    }
+};
